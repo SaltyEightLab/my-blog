f6747206dc80478b52682298b5ca9ac0317eb401
Takuya Yashio <hachiman.hachi8@gmail.com>
2024-02-06 06:07:20 +0900

    supabase実行環境の構築

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import ArticleList from "./components/ArticleList";
 import { getAllArticles } from "@/blogAPI";
+import { supabase } from "@/utils/supabaseClient";
 
 export default async function Home() {
   const articles = await getAllArticles();
-  console.log(articles);
+  console.log(supabase);
 
   return (
     <div className="md:flex">
